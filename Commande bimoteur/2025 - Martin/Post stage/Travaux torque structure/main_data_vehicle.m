987564b302d036edcad134e605c8302a2f380d6a
mcogez-MCZ <martin.cogez@etu.ec-lyon.fr>
2026-06-22 20:35:57 +0200

    Minor modifications to car model (backup)

--- Commande bimoteur/2025 - Martin/Post stage/Travaux torque structure/main_data_vehicle.m
+++ Commande bimoteur/2025 - Martin/Post stage/Travaux torque structure/main_data_vehicle.m
@@ -16,15 +16,16 @@ bf=t/2; %demi largeur avant
 br=t/2; %demi largeur arrière
 Ts=0.001; %période d'échantillonage
 Izz=120; %inertie selon l'axe z, à préciser 
-v0=2; %vitesse initiale pour la simulation
+v0=10; %vitesse initiale pour la simulation
 Crr=0.02; %coeff de résistance au roulement
 Frr=Crr*m*g; %frottements solides
 gyro=1; %0 ou 1, active le terme gyroscopique des PFD, laisser à 1
 LT=1; %0 ou 1, active le calcul des transferts de charge
 delta_skidpad=L/9.125*180/pi; %neutral steer angle pour skidpad en degré
-TV=1; %0 ou 1, active le torque vectoring
+TV=0; %0 ou 1, active le torque vectoring
 TC=0;
 epsilon=0.1; %pour éviter les divisions par 0 dans le calcul du SR
+mu=1.2; %coeff de frottement pneu
 
 
 
